ae0c602ef7d7a2c3343d9d221a6dd561c0894acd
1NVI5IBLE <62433063+1NVI5IBLE@users.noreply.github.com>
2024-11-25 12:30:09 +0000

    Creating the Clear Method

--- Project Files/Treemap.h
+++ Project Files/Treemap.h
@@ -1,9 +1,15 @@
 #pragma once
 #include <iostream>
 #include "BinaryTree.h"
+#include "Pair.h"
 
+template<class K, class V>
 
 class Treemap {
-	
+	BinaryTree<Pair<K, V>> BT;
 
+
+	void clear(){
+		BT.clear;
+	}
 };
