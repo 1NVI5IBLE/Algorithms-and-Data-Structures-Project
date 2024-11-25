cbcbde7dbd5f44ea05317fe4cceea4aefe39fc10
1NVI5IBLE <62433063+1NVI5IBLE@users.noreply.github.com>
2024-11-25 12:44:38 +0000

    Creating Constructor, public methods and destructor

--- Project Files/Treemap.h
+++ Project Files/Treemap.h
@@ -9,7 +9,18 @@ class Treemap {
 	BinaryTree<Pair<K, V>> BT;
 
 
-	void clear(){
-		BT.clear;
-	}
+public:
+	Treemap();
+	Treemap(const Treemap <K, V>& other);
+	Treemap<K, V>& operator=(const Treemap <K, V>& other);
+
+	void clear();
+	void containsKey(K key);
+	V& get(K key);
+	BinaryTree<K> keySet();
+	void put(K key, V value);
+	int size();
+	bool removeKey(K key);
+	V& operator[](K key);
+	~Treemap();
 };
